f8452598f6617f93b94f68b65bf0f22f5e84634a
Ryan Huynh <ryanhuynh@Ryans-MacBook-Air-3.local>
2022-10-26 18:25:02 -0500

    add feature to show legal moves

--- guiJS/main/js/chess_info.js
+++ guiJS/main/js/chess_info.js
@@ -1,31 +1,48 @@
-//This code is from https://chessboardjs.com/examples#5000
-//Example Only Allow Legal Moves
+//This code is from https://chessboardjs.com/examples#5003
 
+//Highlight Legal Moves
 // NOTE: this example uses the chess.js library:
 // https://github.com/jhlywa/chess.js
 
 import { Chess } from './chess.js'
 
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
 var board = null
 var game = new Chess()
-var $status = $('#status')
-var $fen = $('#fen')
-var $pgn = $('#pgn')
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
 
-//Function activates when a piece is dragged
-function onDragStart (source, piece, position, orientation) {
+function removeGreySquares () {
+  $('#main-board .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+  var $square = $('#main-board .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
+function onDragStart (source, piece) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
 
-  // only pick up pieces for the side to move
+  // or if it's not that side's turn
   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
     return false
   }
 }
 
-//Function activates when a piece is dropped
 function onDrop (source, target) {
+  removeGreySquares()
+
   // see if the move is legal
   var move = game.move({
     from: source,
@@ -35,47 +52,33 @@ function onDrop (source, target) {
 
   // illegal move
   if (move === null) return 'snapback'
-
-  updateStatus()
 }
 
-// update the board position after the piece snap
-// for castling, en passant, pawn promotion
+function onMouseoverSquare (square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+
+function onMouseoutSquare (square, piece) {
+  removeGreySquares()
+}
+
 function onSnapEnd () {
   board.position(game.fen())
-}
-
-function updateStatus () {
-  var status = ''
-
-  var moveColor = 'White'
-  if (game.turn() === 'b') {
-    moveColor = 'Black'
-  }
-
-  // checkmate?
-  if (game.in_checkmate()) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.'
-  }
-
-  // draw?
-  else if (game.in_draw()) {
-    status = 'Game over, drawn position'
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move'
-
-    // check?
-    if (game.in_check()) {
-      status += ', ' + moveColor + ' is in check'
-    }
-  }
-
-  $status.html(status)
-  $fen.html(game.fen())
-  $pgn.html(game.pgn())
 }
 
 var config = {
@@ -83,8 +86,8 @@ var config = {
   position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
   onSnapEnd: onSnapEnd
 }
 board = Chessboard('main-board', config)
-
-updateStatus()
